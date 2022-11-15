337a40a321706a164409aedcd8df4e6bf39c1f0a
Notlynoel <leo@notlinoel.com.br>
2022-11-14 22:30:31 -0300

    Adicionando a classe AppQuebecApplication.java

---
+++ app-quebec/src/main/java/br/com/notlinoel/AppQuebecApplication.java
@@ -0,0 +1,14 @@
+package br.com.notlinoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppQuebecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppQuebecApplication.class, args);
+		System.out.println("Hello, world!!");
+	}
+
+}
