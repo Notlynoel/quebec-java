a220a055fbbb1406fdc0f6cb3f05befe2900a9af
Notlynoel <leo@notlinoel.com.br>
2022-11-19 11:48:49 -0300

    Adicionando todos os arquivos do diretório test

---
+++ app-quebec/src/test/java/br/com/notlinoel/AppQuebecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.notlinoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppQuebecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
